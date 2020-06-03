98fef4c010ed756e9f51fa521f1f2add9229ce20
Mathew Gigi George <60754682+mathaigg@users.noreply.github.com>
2020-06-03 21:59:45 +0530

    Add files via upload

---
+++ 6-12-2019/Task 1/task1.py
@@ -0,0 +1,18 @@
+# Printing a Regular String
+x = 'A combination of words that makes a complete sense is called a sentence.'
+print(x)
+print("\n")
+
+# Printing a Paragraph
+y =  '''Sunset is the time of day when our sky meets the outer space solar winds. 
+There are blue, pink, and purple swirls, spinning and twisting, like clouds of 
+balloons caught in a whirlwind. The sun moves slowly to hide behind the line of 
+horizon, while the moon races to take its place in prominence atop the night sky. 
+People slow to a crawl, entranced, fully forgetting the deeds that must still be 
+done. There is a coolness, a calmness, when the sun does set.'''
+print(y)
+print("\n")
+
+#Printing a String of Hindi Language Characters
+z = 'प्रत्येक व्यक्ति को शिक्षा का अधिकार है'
+print(z)
